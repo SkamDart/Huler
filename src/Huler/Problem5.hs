eb6bd2627423a46215388257cba5deb2bbec2248
Cameron Dart <cdart@anduril.com>
2019-12-29 10:45:07 -0800

    problem 5 (bruteforce)

--- src/Huler/Problem5.hs
+++ src/Huler/Problem5.hs
@@ -1,4 +1,23 @@
 module Huler.Problem5 (problem5) where
 
+import Huler.Toolbox (takeFirst)
+
+import Debug.Trace
+
+-- Problem 5
+--
+-- 2520 is the smallest number that can be divided by each of the numbers
+-- from 1 to 10 without any remainder.
+--
+-- What is the smallest positive number that is evenly divisible
+-- by all of the numbers from 1 to 20?
 problem5 :: IO ()
-problem5 = putStrLn "Undefined"
+problem5 = bruteforce
+-- problem5 = putStrLn $ show ans
+  -- where ans = "foo"
+
+bruteforce :: IO ()
+bruteforce = putStrLn $ show ans
+    where ans = scm [1..20]
+          scm xss = takeFirst (divAll xss) [20..]
+          divAll divs k = trace ("divAll " ++ show k) (all (\x -> k `mod` x == 0) divs)
