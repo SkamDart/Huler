d45038c235e0a8072e932fe0a6c14955ec5bd3ee
Cameron Dart <cdart@anduril.com>
2020-01-05 15:22:47 -0800

    add problem generation command using handlebars

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import qualified Data.Map.Strict as M
 
+import qualified Data.Text       as T
+import qualified Data.Text.IO    as T
+
 import System.Environment
+
 import System.Exit
 
+import Text.Mustache
+
 import Huler.Solutions (intToAns)
+
+
+data Problem = Problem { number :: String }
+
+instance ToMustache Problem where
+  toMustache problem = object [ "number" ~> number problem ]
 
 usage :: IO ()
 usage = putStrLn "enter problem number to generate solution for"
@@ -17,9 +30,23 @@ run n = soln
       soln = case M.lookup num intToAns of Nothing -> putStrLn "no answer found"
                                            Just f  -> f
 
+-- | gen creates a new file from our mustache template
+gen :: String -> IO ()
+gen problemNumber = do
+  let searchSpace = ["." , "templates"]
+      problemTemplateFilename = "problem.mustache"
+      problemFilename = "src/Huler/Problem" ++ problemNumber ++ ".hs"
+  mTemplate <- automaticCompile searchSpace problemTemplateFilename
+  case mTemplate of
+    Left err       -> print err
+    Right template -> do
+      let fileContents = substitute template $ Problem problemNumber
+      T.writeFile problemFilename fileContents
+
 parseAndRun :: [String] -> IO ()
-parseAndRun argv = case argv of [num] -> run num
-                                _     -> usage
+parseAndRun argv = case argv of [num]        -> run num
+                                ["gen", num] -> gen num
+                                _            -> usage
 
 main :: IO ()
 main = getArgs >>= parseAndRun
